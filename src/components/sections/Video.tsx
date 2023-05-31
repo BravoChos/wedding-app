7164992708d8148a056240775c93b6988b8d9040
BravoChos <bravochos@gmail.com>
2023-05-31 19:59:23 +0900

    add section and header

---
+++ src/components/sections/Video.tsx
@@ -0,0 +1,21 @@
+// import classNames from 'classnames/bind'
+
+// import styles from './Video.module.scss'
+import Section from '../shared/Section'
+
+// const cx = classNames.bind(styles)
+
+function Video() {
+  return (
+    <Section>
+      {/* <Section className={cx('container')}> */}
+      Video
+      {/* <video autoPlay loop muted poster="/assets/poster.jpg">
+        <source src="/assets/main.webm" type="video/webm" />
+        <source src="/assets/main.mp4" type="video/mp4" />
+      </video> */}
+    </Section>
+  )
+}
+
+export default Video
