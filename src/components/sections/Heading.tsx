6bd71cacd75a7cc25eb7e49b01b7d0292640a601
BravoChos <bravochos@gmail.com>
2023-06-01 12:04:32 +0900

    apply data-fns and add wedding model

--- src/components/sections/Heading.tsx
+++ src/components/sections/Heading.tsx
@@ -1,33 +1,31 @@
 import classNames from 'classnames/bind'
-// import { format, parseISO, getDay } from 'date-fns'
+import { format, parseISO, getDay } from 'date-fns'
 
 import styles from './Heading.module.scss'
 import Section from '@shared/Section'
 
 const cx = classNames.bind(styles)
 
-// const days = [
-//   'Sunday',
-//   'Monday',
-//   'Tuesday',
-//   'Wednesday',
-//   'Thursday',
-//   'Friday',
-//   'Saturday',
-// ]
+const DAYS = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
 
-function Heading() {
-  // const weddingDate = parseISO(date)
+function Heading({ date }: { date: string }) {
+  const weddingDate = parseISO(date)
 
-  // const title = format(weddingDate, 'yy.MM.dd')
-  // const subTitle = days[getDay(weddingDate)]
+  const title = format(weddingDate, 'yy.MM.dd')
+  const subTitle = DAYS[getDay(weddingDate)]
 
   return (
     <Section className={cx('container')}>
-      <div className={cx('txt-date')}>23.12.27</div>
-      <div className={cx('txt-day')}>Wednesday</div>
-      {/* <div className={cx('txt-date')}>{title}</div>
-      <div className={cx('txt-day')}>{subTitle}</div> */}
+      <div className={cx('txt-date')}>{title}</div>
+      <div className={cx('txt-day')}>{subTitle}</div>
     </Section>
   )
 }
